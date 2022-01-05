7415861845b1b8bea23b22795372ecd16dde29f2
pierrecampos <cdias.pierre@gmail.com>
2022-01-05 20:14:04 -0300

    Double Jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     private Rigidbody2D rig;
+    public Animator anim;
     public float speed;
+    public float jumpForce;
+    private bool isJumping;
+    private bool doubleJump;
 
     void Start() {
         rig = GetComponent<Rigidbody2D>();
     }
 
     void Update() {
-
+        Jump();
     }
 
     void FixedUpdate() {
@@ -20,10 +24,45 @@ public class Player : MonoBehaviour {
     }
 
     void Move() {
-        float movement = Input.GetAxis("Horizontal");        
+        float movement = Input.GetAxis("Horizontal");
         rig.velocity = new Vector2(movement * speed, rig.velocity.y);
 
-        if (movement > 0) transform.eulerAngles = new Vector3(0, 0, 0);
-        if (movement < 0) transform.eulerAngles = new Vector3(0, 180, 0);
+        if (movement > 0) {
+            if (!isJumping) {
+                anim.SetInteger("transition", 1);
+            }
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        if (movement < 0) {
+            if (!isJumping) {
+                anim.SetInteger("transition", 1);
+            }
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+
+        if (movement == 0 && !isJumping) {
+            anim.SetInteger("transition", 0);
+
+        }
+    }
+
+    void Jump() {
+        if (Input.GetButtonDown("Jump")) {
+            if (!isJumping) {
+                anim.SetInteger("transition", 2);
+                rig.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                isJumping = true;
+                doubleJump = true;
+            } else if (doubleJump) {
+                rig.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                doubleJump = false;
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col) {
+        if (col.gameObject.layer == 6) {
+            isJumping = false;
+        }
     }
 }
