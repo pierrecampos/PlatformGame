c4f192f0a67b534c5fb2e18028fc8fdc26e66386
pierrecampos <cdias.pierre@gmail.com>
2022-01-05 21:18:34 -0300

    Player Attack

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,10 +6,15 @@ public class Player : MonoBehaviour {
 
     private Rigidbody2D rig;
     public Animator anim;
+    public Transform point;
+    public float radius;
+
     public float speed;
     public float jumpForce;
+
     private bool isJumping;
     private bool doubleJump;
+    private bool isAttacking;
 
     void Start() {
         rig = GetComponent<Rigidbody2D>();
@@ -17,6 +22,7 @@ public class Player : MonoBehaviour {
 
     void Update() {
         Jump();
+        Attack();
     }
 
     void FixedUpdate() {
@@ -28,21 +34,20 @@ public class Player : MonoBehaviour {
         rig.velocity = new Vector2(movement * speed, rig.velocity.y);
 
         if (movement > 0) {
-            if (!isJumping) {
+            if (!isJumping && !isAttacking) {
                 anim.SetInteger("transition", 1);
             }
             transform.eulerAngles = new Vector3(0, 0, 0);
         }
         if (movement < 0) {
-            if (!isJumping) {
+            if (!isJumping && !isAttacking) {
                 anim.SetInteger("transition", 1);
             }
             transform.eulerAngles = new Vector3(0, 180, 0);
         }
 
-        if (movement == 0 && !isJumping) {
+        if (movement == 0 && !isJumping && !isAttacking) {
             anim.SetInteger("transition", 0);
-
         }
     }
 
@@ -58,6 +63,28 @@ public class Player : MonoBehaviour {
                 doubleJump = false;
             }
         }
+    }
+
+    void Attack() {
+        if (Input.GetButtonDown("Fire1") && !isAttacking) {
+            isAttacking = true;
+            anim.SetInteger("transition", 3);
+
+            Collider2D hit = Physics2D.OverlapCircle(point.position, radius);
+
+            if (hit) Debug.Log(hit.name);
+            StartCoroutine(OnAttack());
+        }
+
+    }
+
+    IEnumerator OnAttack() {
+        yield return new WaitForSeconds(.33f);
+        isAttacking = false;
+    }
+
+    void OnDrawGizmos() {
+        Gizmos.DrawWireSphere(point.position, radius);
     }
 
     void OnCollisionEnter2D(Collision2D col) {
