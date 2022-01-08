db9733e5a78ad9cd8ccd8b77b0c068bf4e0b97d5
pierrecampos <cdias.pierre@gmail.com>
2022-01-07 22:14:32 -0300

    Goblin Animations

--- Assets/Scripts/EnemyGoblin.cs
+++ Assets/Scripts/EnemyGoblin.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class EnemyGoblin : MonoBehaviour {
 
     private Rigidbody2D rig;
+    private Animator anim;
+
     public Transform point;
+    public Transform behindPoint;
+
     public bool isFront;
     public bool isRight;
     public float stopDistance;
@@ -16,11 +20,8 @@ public class EnemyGoblin : MonoBehaviour {
 
     void Start() {
         rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
         OnMove();
-    }
-
-    void Update() {
-
     }
 
     void FixedUpdate() {
@@ -31,33 +32,45 @@ public class EnemyGoblin : MonoBehaviour {
     void GetPlayer() {
         RaycastHit2D hit = Physics2D.Raycast(point.position, direction, maxVision);
 
-        isFront = true;
-
         if (hit.collider != null && hit.transform.CompareTag("Player")) {
+            isFront = true;
+            
             float distance = Vector2.Distance(transform.position, hit.transform.position);
+            
             if (distance <= stopDistance) {
                 isFront = false;
+                anim.SetInteger("transition", 2);                
                 rig.velocity = Vector2.zero;
                 hit.transform.GetComponent<Player>().OnHit();
             }
         }
+
+        RaycastHit2D behindHit = Physics2D.Raycast(behindPoint.position, -direction, maxVision);
+        if (behindHit.collider != null && behindHit.transform.CompareTag("Player")) {
+            isRight = !isRight;
+        }
     }
 
     void OnMove() {
+        Vector2 vel;
+        if (isRight) {
+            transform.eulerAngles = new Vector2(0, 0);
+            vel = new Vector2(speed, rig.velocity.y);
+            direction = Vector2.right;
+        } else {
+            transform.eulerAngles = new Vector2(0, 180);
+            vel = new Vector2(-speed, rig.velocity.y);
+            direction = Vector2.left;
+        }
+
         if (isFront) {
-            if (isRight) {
-                transform.eulerAngles = new Vector2(0, 0);
-                rig.velocity = new Vector2(speed, rig.velocity.y);
-                direction = Vector2.right;
-            } else {
-                transform.eulerAngles = new Vector2(0, 180);
-                rig.velocity = new Vector2(-speed, rig.velocity.y);
-                direction = Vector2.left;
-            }
+            anim.SetInteger("transition", 1);
+            rig.velocity = vel;
         }
     }
 
     void OnDrawGizmos() {
         Gizmos.DrawRay(point.position, direction * maxVision);
+        Gizmos.DrawRay(behindPoint.position, -direction * maxVision);
     }
 }
