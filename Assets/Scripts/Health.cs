a3d4885633d556a1e5d05e60c6af99158bda040c
pierrecampos <cdias.pierre@gmail.com>
2022-01-09 17:39:23 -0300

    Player Health HUD

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour {
+
+    [SerializeField]
+    private int healthCount;
+
+    public Player player;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+    public Image[] hearts;
+
+
+    void Update() {
+
+        for (int x = 0; x < hearts.Length; x++) {
+            hearts[x].sprite = x < player.health ? fullHeart : emptyHeart;
+        }
+
+        for (int x = 0; x < hearts.Length; x++) {
+            hearts[x].enabled = x < healthCount;
+        }
+
+    }
+}
