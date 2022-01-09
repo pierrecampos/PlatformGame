0f88f5e85db8a950f11085bff98b6286fd889c83
pierrecampos <cdias.pierre@gmail.com>
2022-01-09 19:16:14 -0300

    Menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+    public void StartGame() {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame() {
+        Application.Quit();
+    }
+}
